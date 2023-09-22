3ab863c1ddd38797c523be73cab4ddbfe7459693
Adam Darmawansa <adamdarmawansa439@gmail.com>
2023-09-22 18:35:56 +0700

    Add homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,25 +30,129 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 255, 255, 255),
+      backgroundColor: const Color.fromARGB(0, 255, 255, 255),
       body: SafeArea(
           child: Center(
         child: Column(
-          children: [
-            Image.asset(
-              'lib/image/WhatsApp Image 2023-09-16 at 14.44.45.jpg',
-              height: 300,
-              width: 300,
+          children: <Widget>[
+            Container(
+              width: 220,
+              height: 220,
+              margin: const EdgeInsets.only(top: 100, right: 15),
+              child: Image.asset(
+                  "lib/image/WhatsApp Image 2023-09-16 at 14.44.45.jpg"),
             ),
-            const Spacer(),
-            Lottie.asset(
-              'lib/animation/animation_lmm7e6qh.json',
-              height: 500,
-              width: 500,
+            const SizedBox(
+              height: 20,
+            ), // ignore: sized_box_for_whitespace
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                const Text(
+                  'Make A ',
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                DefaultTextStyle(
+                  style: const TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  child: AnimatedTextKit(
+                    animatedTexts: [
+                      TypewriterAnimatedText(
+                        'Great',
+                        speed: const Duration(milliseconds: 150),
+                      ),
+                      TypewriterAnimatedText(
+                        'Best',
+                        speed: const Duration(milliseconds: 150),
+                      ),
+                      TypewriterAnimatedText(
+                        'Nice',
+                        speed: const Duration(milliseconds: 150),
+                      ),
+                      TypewriterAnimatedText(
+                        'Good',
+                        speed: const Duration(milliseconds: 150),
+                      ),
+                    ],
+                    isRepeatingAnimation: true,
+                    repeatForever: true,
+                  ),
+                ),
+                const Text(
+                  'day for your fish',
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              child: SizedBox(
+                width: 220,
+                height: 40,
+                child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const LoginPage()));
+                    },
+                    child: const Text(
+                      "Mulai",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                      ),
+                    )),
+              ),
             ),
           ],
         ),
       )),
     );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: const Color.fromARGB(0, 255, 255, 255),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              children: [
+                Container(
+                  width: 250,
+                  height: 250,
+                  margin: const EdgeInsets.only(bottom: 10),
+                  child: Lottie.asset("lib/animation/animation_lmm7e6qh.json"),
+                ),
+                const SizedBox(
+                  child: SizedBox(
+                    child: Center(
+                      child: TextField(),
+                    ),
+                  ),
+                ),
+                // Tombol Mulai
+              ],
+            ),
+          ),
+        ));
   }
 }
